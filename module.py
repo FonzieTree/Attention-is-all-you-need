635838ea56cd6d109815d397791658548174dff0
Shuming Fang <fangshuming519@gmail.com>
2017-12-12 09:03:22 +0800

    Update module.py

--- module.py
+++ module.py
@@ -3,151 +3,206 @@
 # fangshuming519@gmail.com.
 # https://github.com/FonzieTree
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
-import numpy as np
+from __future__ import print_function
 from hyperparams import Hyperparams as hp
-def normalize(inputs, 
-              beta,
-              gamma):
-    # Layer mean
-    params_shape = inputs.shape[-1]
-    mean = np.mean(inputs, axis=-1, keepdims=True)
-    variance = np.var(inputs, axis=-1, keepdims=True)
-    normalized = (inputs - mean) / ((variance + 1e-8) ** (.5))
-    outputs = gamma * normalized + beta
+from data_load import load_train_data, get_batch_data, load_de_vocab, load_en_vocab
+import os, codecs
+from tqdm import tqdm
+import numpy as np
+from modules2 import *
+np.random.seed(0)
+print('loading vocabulary...')
+de2idx, idx2de = load_de_vocab()
+en2idx, idx2en = load_en_vocab()
+print('done')
+print('loading datasets...')
+X, Y = load_train_data()
+print('done')
+num_samples = X.shape[0]
+dff = 2048 # dimention of inner layer
+# Some hyperparameters
+reg = 0.1 # regularization strength
+epoch = 10000
+lr = 0.0000001
+
+# Encoder parameters
+encoder_w1 = 0.001*np.random.randn(4,hp.hidden_units,hp.hidden_units)
+encoder_w2 = 0.001*np.random.randn(1,dff)
+encoder_w3 = 0.001*np.random.randn(1,hp.hidden_units)
+lookup_table1 = 0.001*np.random.randn(len(de2idx), hp.hidden_units)
+# Decoder parameters
+decoder_w1 = 0.001*np.random.randn(4,hp.hidden_units,hp.hidden_units)
+decoder_w2 = 0.001*np.random.randn(4,hp.hidden_units,hp.hidden_units)
+decoder_w3 = 0.001*np.random.randn(1,dff)
+decoder_w4 = 0.001*np.random.randn(1,hp.hidden_units)
+decoder_w5 = 0.001*np.random.randn(hp.hidden_units,len(en2idx))
+lookup_table2 = 0.001*np.random.randn(len(en2idx), hp.hidden_units)
+
+for i in range(epoch):
+    select = np.random.randint(0,num_samples,hp.batch_size)
+    x = X[select, :]
+    y = Y[select, :]
+    # Forward path
+    # Encoder
+    encoder1 = embedding(x,lookup_table1,num_units=hp.hidden_units,scale=True)
+    position_encoder = positional_encoding(x,num_units=hp.hidden_units,zero_pad=True,scale=True)
+    encoder2 = encoder1 + position_encoder
+    encoder3 = multihead_attention(queries=encoder2,keys=encoder2,attention_w=encoder_w1)
+    encoder4 = normalize(inputs = encoder3[5])
+    encoder5 = feedforward(encoder4[2],encoder_w2,encoder_w3)
+    encoder6 = normalize(encoder5[4])
+    # Decoder
+    decoder_inputs = np.concatenate((np.ones((hp.batch_size,1), dtype=int)*2, y[:,:-1]), axis=1) # 2:<S>
+    decoder1 = embedding(decoder_inputs,lookup_table2,num_units=hp.hidden_units,scale=True)
+    position_decoder = positional_encoding(decoder_inputs,num_units=hp.hidden_units,zero_pad=True,scale=True)
+    decoder2 = decoder1 + position_decoder
+    decoder3 = multihead_attention(queries=decoder2,keys=decoder2,attention_w=decoder_w1)
+    decoder4 = normalize(inputs = decoder3[5])
+    decoder5 = multihead_attention(queries = decoder4[2],keys=encoder6[2],attention_w=decoder_w2)
+    decoder6 = normalize(inputs = decoder5[5])
+    decoder7 = feedforward(inputs = decoder6[2], w1 = decoder_w3, w2 = decoder_w4)
+    decoder8 = normalize(inputs = decoder7[4])
+    scores = np.dot(decoder8[2], decoder_w5)
+    #scores = label_smoothing(scores)
+    exp_scores = np.exp(scores)
+    probs = exp_scores/np.sum(exp_scores,axis=-1,keepdims=True)
+    print(np.max(scores))
+    # Backpropegation   
+    # compute the loss: average cross-entropy loss and regularization
+    correct_logprobs = -np.log(np.array([probs[j][range(hp.maxlen),y[j]] for j in range(hp.batch_size)]))
+    data_loss = np.sum(correct_logprobs)/hp.batch_size
+    #reg_loss = 0.5*reg*(np.sum(encoder_w1*encoder_w1) + np.sum(encoder_w2*encoder_w2) + np.sum(encoder_w3*encoder_w3) +
+    #np.sum(lookup_table1*lookup_table1) + np.sum(decoder_w1*decoder_w1) + np.sum(decoder_w2*decoder_w2) + np.sum(decoder_w3*decoder_w3) +
+    #np.sum(decoder_w4*decoder_w4) + np.sum(decoder_w5*decoder_w5) + np.sum(lookup_table2*lookup_table2))    
+    #loss = data_loss + reg_loss
+    #print("iteration %d: data_loss %f" % (i, reg_loss))
+    print(data_loss)
+    # compute the gradient on scores
+    dscores = probs
+    for j in range(hp.batch_size):
+        dscores[j][range(hp.maxlen),y[j]] -=1
     
-    return [mean, variance, normalized, outputs]
-def de_normalize(beta,
-		 gamma,
-		 mean,
-		 variance,
-		 normalized,
-		 outputs):
-    dinputs = mean + ((variance + 1e-8)**.5)*(outputs - beta)/gamma
-    dgamma = (outputs - beta)/normalized
-    dbeta = (outputs - gamma * normalized)
-    dgamma = np.sum(dgamma, axis = (0,1))/(dgamma.shape[0]*dgamma.shape[1])
-    dbeta= np.sum(dbeta, axis = (0,1))/(dbeta.shape[0]*dbeta.shape[1])
-    return [dbeta, dgamma, dinputs]
+    dscores /= hp.batch_size
+    ddecoder8 = np.dot(dscores, decoder_w5.T)
+    ddecoder_w5 = np.array([np.dot(decoder8[2][j,:,:].T,dscores[j,:,:]) for j in range(hp.batch_size)])
+    ddecoder_w5 = np.sum(ddecoder_w5, axis=0)/hp.batch_size
+    # de_layer normalization
+    ddecoder7 = de_normalize(decoder8[0], decoder8[1], ddecoder8)
 
-def embedding(inputs, 
-              lookup_table, 
-              num_units, 
-              zero_pad=True, 
-              scale=True,
-              reuse=None):
-    if zero_pad:
-        lookup_table = np.concatenate((np.zeros((1,num_units), dtype=int), lookup_table[1:,:]), axis=0) 
-    outputs = lookup_table[inputs]
-        
-    if scale:
-        outputs = outputs * (num_units ** 0.5) 
-            
-    return outputs
+    # dense backward
+    ddecoder6 = backward(inputs = decoder6[2],
+                         w1 = decoder_w3,
+	                 w2 = decoder_w4,
+			 index = decoder7[0],
+			 outputs1 = decoder7[1],
+			 outputs2 = decoder7[2],
+			 outputs3 = decoder7[3],
+			 outputs = ddecoder7)
+    ddecoder_w3 = ddecoder6[0].reshape(1,2048)
+    ddecoder_w4 = ddecoder6[1].reshape(1,512)
     
 
-def positional_encoding(inputs,
-                        num_units,
-                        zero_pad=True,
-                        scale=True):
-    N, T = inputs.shape
-    position_ind = np.tile(np.array(range(T)),(N,1))
+    # de_layer normalization
+    ddecoder5 = de_normalize(mean = decoder6[0],
+			     variance = decoder6[1],
+	         	     outputs = ddecoder6[2])
 
-    # First part of the PE function: sin and cos argument
-    position_enc = np.array([[pos/np.power(10000, 2.*i/num_units) for i in range(num_units)]
-        for pos in range(T)])
+    # multi_head attention3 backward
+    dmulti3 = de_multihead_attention(outputs1 = decoder5[0],
+				     outputs2 = decoder5[1],
+				     outputs5 = decoder5[2],
+				     outputs6 = decoder5[3],
+				     outputs7 = decoder5[4],
+                                     outputs = ddecoder5,
+                                     Q = decoder5[6],
+				     K = decoder5[7],
+				     V = decoder5[8],
+                                     queries = decoder5[9],
+                                     keys = decoder5[10], 
+				     attention_w = decoder_w2)
+    ddecoder4 = dmulti3[0]
+    dencoder6 = dmulti3[1]
+    ddecoder_w2 = dmulti3[2]
 
-    # Second part, apply the cosine to even columns and sin to odds.
-    position_enc[:, 0::2] = np.sin(position_enc[:, 0::2])  # dim 2i
-    position_enc[:, 1::2] = np.cos(position_enc[:, 1::2])  # dim 2i+1
+    # de_layer normalization
+    ddecoder3 = de_normalize(mean = decoder4[0],
+                             variance = decoder4[1],
+                             outputs = ddecoder4)
 
-    # Convert to a tensor
-    lookup_table = position_enc
 
-    if zero_pad:
-        lookup_table = np.concatenate((np.zeros((1,num_units), dtype=int), lookup_table[1:,:]), axis=0)
-    outputs = lookup_table[position_ind]
+    # multi_head attention2 backward
+    dmulti2 = de_multihead_attention(outputs1 = decoder3[0],
+				     outputs2 = decoder3[1],
+				     outputs5 = decoder3[2],
+				     outputs6 = decoder3[3],
+				     outputs7 = decoder3[4],
+                                     outputs = ddecoder3,
+				     Q = decoder3[6],
+				     K = decoder3[7],
+				     V = decoder3[8],
+                                     queries = decoder3[9],
+                                     keys = decoder3[10],
+				     attention_w = decoder_w1)
+    ddecoder2 = (dmulti2[0] + dmulti2[1])/2
+    ddecoder_w1 = dmulti2[2]
 
-    if scale:
-        outputs = outputs * (num_units ** 0.5)
+    # de_decoder_embedding
+    ddecoder1 = ddecoder2
+    ddecoder1 = ddecoder1/(hp.hidden_units ** 0.5)
+    lookup_table2[decoder_inputs] += -lr*ddecoder1
 
-    return outputs
+    # de_layer normalization
+    dencoder5 = de_normalize(encoder6[0], encoder6[1], dencoder6)
 
-def multihead_attention(queries,
-                        keys,
-                        attention_w):
-    Q = np.dot(queries, attention_w[0,:,:])
-    K = np.dot(keys, attention_w[1,:,:])
-    V = np.dot(keys, attention_w[2,:,:])
-    # Multiplication
-    outputs1 = np.array([np.dot(Q[i,:,:], K[i,:,:].T) for i in range(hp.batch_size)])
-    # Scale
-    outputs2 = outputs1/(K.shape[1]** 0.5)
-    # SoftMax
-    index = (outputs2<=0)
-    outputs3 = np.maximum(0,outputs2)
-    outputs4 = np.array([np.dot(outputs3[i,:,:], V[i,:,:]) for i in range(hp.batch_size)])
-    outputs5 = np.array([np.dot(outputs4[i,:,:], attention_w[3,:,:]) for i in range(hp.batch_size)])
-    # Add residual connections
-    outputs6 = outputs5 + queries
-    return [index, outputs1, outputs2, outputs3,  outputs4,  outputs5,  outputs6, Q, K, V, queries, keys]
 
-def de_multihead_attention(index, outputs1, outputs2, outputs3,  outputs4,  outputs5, outputs, Q, K, V, queries, keys, attention_w):
-    dattention_w = np.zeros((attention_w.shape)) 
-    doutputs5 = outputs
-    doutputs4 = np.array([np.dot(doutputs5[i,:,:], dattention_w[3,:,:].T) for i in range(hp.batch_size)])
-    dV = np.array([np.dot(outputs3[i,:,:].T, doutputs4[i,:,:]) for i in range(hp.batch_size)])
-    doutputs3 = np.array([np.dot(doutputs4[i,:,:], V[i, :, :].T) for i in range(hp.batch_size)])
-    doutputs3[index] = 0
-    doutputs2 = doutputs3
-    doutputs1 = doutputs2*(K.shape[1]** 0.5)
-    dQ = np.array([np.dot(doutputs1[i, :, :], K[i,:,:]) for i in range(hp.batch_size)])
-    dK = np.array([np.dot(Q[i, :, :].T, doutputs1[i, :, :]).T for i in range(hp.batch_size)])
-    dqueries = np.dot(dQ, attention_w[0, :, :].T)
-    dkeys = (np.dot(dK, attention_w[1, :, :]) + np.dot(dV, attention_w[2, :, :]))/2
-    d0 = np.array([np.dot(queries[i, :, :].T, Q[i, :, :]) for i in range(hp.batch_size)])/hp.batch_size
-    dattention_w[0, :, :] = np.sum(d0, axis = 0)
-    d1 = np.array([np.dot(keys[i, :, :].T, K[i, :, :]) for i in range(hp.batch_size)])/hp.batch_size
-    dattention_w[1, :, :] = np.sum(d1, axis = 0)
-    d2 = np.array([np.dot(keys[i, :, :].T, V[i, :, :]) for i in range(hp.batch_size)])/hp.batch_size
-    dattention_w[2, :, :] = np.sum(d2, axis = 0)
-    d3 = np.array([np.dot(outputs4[i,:,:].T, doutputs5[i,:,:]) for i in range(hp.batch_size)])/hp.batch_size
-    dattention_w[3, :, :] = np.sum(d3, axis = 0)
-    return [dqueries, dkeys, dattention_w]
+    # backward
+    dencoder4 = backward(inputs = encoder4[2],
+                         w1 = encoder_w2,
+	                 w2 = encoder_w3,
+			 index = encoder5[0],
+			 outputs1 = encoder5[1],
+			 outputs2 = encoder5[2],
+			 outputs3 = encoder5[3],
+			 outputs = dencoder5)
+    dencoder_w2 = dencoder4[0].reshape(1, 2048)
+    dencoder_w3 = dencoder4[1].reshape(1, 512)
 
-def feedforward(inputs,
-                w1,
-                w2):
-    outputs1 = np.dot(inputs,w1)
-    index = (outputs1<=0)
-    outputs2 = np.maximum(0,outputs1)
-    outputs3 = np.dot(outputs2,w2)
-    outputs4 = outputs3 + inputs    
-    return [index, outputs1, outputs2, outputs3, outputs4]
+    # de_layer normalization
+    dencoder3 = de_normalize(encoder4[0], encoder4[1], dencoder4[2])
 
-def backward(inputs,
-             w1,
-	     w2,
-             index,
-             outputs1,
-             outputs2,
-             outputs3,
-             outputs):
-    doutputs3 = outputs
-    doutputs2 = np.dot(doutputs3, w2.T)
+    # multi_head attention1 backward
+    dmulti1 = de_multihead_attention(outputs1 = encoder3[0],
+				     outputs2 = encoder3[1],
+				     outputs5 = encoder3[2],
+				     outputs6 = encoder3[3],
+				     outputs7 = encoder3[4],
+                                     outputs = dencoder3,
+				     Q = encoder3[6],
+				     K = encoder3[7],
+				     V = encoder3[8],
+                                     queries = encoder3[9],
+                                     keys = encoder3[10],
+				     attention_w = encoder_w1)
+    dencoder2 = (dmulti1[0] + dmulti1[1])/2
+    dencoder_w1 = dmulti1[2]
+    # de_encoder_embedding
+    dencoder1 = dencoder2
+    dencoder1 = dencoder1/(hp.hidden_units ** 0.5)
+    lookup_table1[x] += -lr*dencoder1
+
+    # parameter update
+    # Encoder parameters
     
-    dw2 = np.array([np.dot(outputs2[i, :, :].T, doutputs3[i, :, :]) for i in range(hp.batch_size)])/hp.batch_size
-    dw2 = np.sum(dw2, axis = 0)
-    doutputs2[index] = 0
-    doutputs1 = doutputs2
-    
-    dw1 = np.array([np.dot(inputs[i, :, :].T, doutputs1[i, :, :]) for i in range(hp.batch_size)])/hp.batch_size
-    dw1 = np.sum(dw1, axis = 0)
-    
-    dinputs = np.dot(doutputs1, w1.T)
-    dinputs = dinputs + outputs
-    return [dw1, dw2, dinputs]
+    encoder_w1 += -lr*dencoder_w1
+    encoder_w2 += -lr*dencoder_w2
+    encoder_w3 += -lr*dencoder_w3
 
-def label_smoothing(inputs, epsilon=0.1):
-    K = inputs.get_shape().as_list()[-1] # number of channels
-    return ((1-epsilon) * inputs) + (epsilon / K)
+
+    # Decoder parameters
+    decoder_w1 += -lr*ddecoder_w1
+    decoder_w2 += -lr*ddecoder_w2
+    decoder_w3 += -lr*ddecoder_w3
+    decoder_w4 += -lr*ddecoder_w4
+    decoder_w5 += -lr*ddecoder_w5
+
+    print(i, ' round finished')
